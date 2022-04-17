8c0171d11362eca61e4de1add99ac7e92e2ce2e5
annamolaohein <annamo@nexbe.sg>
2022-04-17 23:29:58 +0630

    conflip using emotion css

--- src/component/CoinFlip.js
+++ src/component/CoinFlip.js
@@ -1,24 +1,80 @@
 import { useState } from "react";
 import "./CoinFlip.css";
+import styled from "@emotion/styled";
 
 export const CoinFlip = () => {
+
   const [coin, setCoin] = useState("");
   const coinFlip = () => {
     const random = Math.random() * 10;
     Math.floor(random % 2) === 0 ? setCoin("HEAD") : setCoin("TAIL");
   };
   return (
-    <div className="CoinFlip">
+    <Div className="CoinFlip">
+      {/* <style>
+        {
+          `
+            .CoinFlip #coin {
+              width: 100px;
+              height: 100px;
+              margin: auto;
+              position: relative;
+            }
+            .CoinFlip .tail {
+              background-color: red;
+              text-align: center;
+              color: white;
+            }
+            .CoinFlip .head {
+              background-color: green;
+              text-align: center;
+              color: white;
+            }
+            `
+        }
+      </style> */}
       {/* <h1>Function Component CoinFlip</h1> */}
-      <h1>CoinFlip</h1>
+      <H1>CoinFlip</H1>
       <div id="coin">
         <div className={coin === "TAIL" ? "tail" : "head"}>
-          <h2>{coin}</h2>
+          <H2>{coin}</H2>
         </div>
       </div>
-      <button id="btn" onClick={coinFlip}>
+      <Button id="btn" onClick={coinFlip}>
         Flip Coin
-      </button>
-    </div>
+      </Button>
+    </Div>
   );
 };
+const Button = styled.button`
+color:darkblue;
+padding: 10px 20px;
+font-size: 0.5;
+font-weight: bold;
+&:hover{color: darkcyan;}`
+
+const H1 = styled.h1`
+color: darkcyan;`
+
+const H2 = styled.h2`
+ font-family: "Times New Roman", Times, serif;`
+
+const Div = styled.div`
+& #coin{
+width: 100px;
+ height: 100px;
+ margin: auto;
+ position: relative;
+}
+
+ & .tail {
+  background-color: red;
+  text-align: center;
+  color: white;
+  }
+  & .head {
+  background-color: green;
+  text-align: center;
+  color: white;
+  }
+ `
